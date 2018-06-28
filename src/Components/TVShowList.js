f111b388b6d08daf9e03b30b123dbb9be74bd359
Garry Clerge <clergeg@gmail.com>
2018-06-28 14:56:12 -0400

    first commit

--- src/Components/TVShowList.js
+++ src/Components/TVShowList.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import Adapter from '../Adapter';
 
 class TVShowList extends Component {
+  state = {
+    shows: [],
+  }
+
+
+  componentDidMount = () => {
+    Adapter.getShows().then(resp => {
+      this.setState({
+        shows: resp
+      })
+    })
+  }
 
 
   render() {
     return (
       <div className="tvShowList">
-
+        Add Code here
       </div>
     );
   }
